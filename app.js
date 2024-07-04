d49f725143a4363610578ce498ae031f673064ec
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-04 16:25:58 +0500

    implemented animation

--- app.js
+++ app.js
@@ -2,24 +2,79 @@ var tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#home",
     start: "top top",
-    markers: true,
+    // markers: true,
     scrub: 1,
     pin: true,
   },
 });
 
-tl.to("#circle #circle-bottom img", {
-  rotate: "-180deg",
-  duration: 1.5,
-  stagger: 0.1,
-  // scale: 0,
-  ease: "Power1.easeInOut",
-}).to("#circle #circle-top img", {
-  scale: 0,
-  ease: "Power1.easeInOut",
-  duration: 1,
-  stagger: 0.1,
-});
+tl.to(
+  "#circle #circle-bottom img",
+  {
+    rotate: "-180deg",
+    duration: 1,
+    stagger: 0.1,
+    scale: 0,
+    ease: "Power1.easeInOut",
+  },
+  "hello"
+)
+  .to(
+    "#circle #circle-top img",
+    {
+      scale: 0,
+      ease: "Power1.easeInOut",
+      duration: 1,
+      stagger: 0.1,
+    },
+    "hello"
+  )
+  .to(
+    "#center-img #c-img img",
+    {
+      scale: 0,
+      ease: "Power1.easeInOut",
+      duration: 1,
+      stagger: 0.1,
+    },
+    "hello"
+  )
+  .to(
+    "#center-img h5",
+    {
+      opacity: 0,
+      // duration: 1,
+      stagger: 0.1,
+      ease: "Power1.easeInOut",
+    },
+    "hello"
+  )
+  .to(
+    "#circle",
+    {
+      scale: 0.6,
+      // duration: 1,
+      ease: "Power1.easeInOut",
+    },
+    "hello"
+  )
+  .to(
+    "#semi-circle",
+    {
+      scale: 0,
+      ease: "Power1.easeInOut",
+    },
+    "hello2"
+  )
+  .to(
+    "#circle",
+    {
+      scale: 0,
+      // duration: 1,
+      ease: "Power1.easeInOut",
+    },
+    "hello2"
+  );
 
 // let img = document.querySelector("#semi-circle");
 // img.addEventListener("mousemove", () => {
