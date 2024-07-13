4b470c690f83cbf3084e5edab910d2010466ab2c
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-13 22:53:58 +0500

    solving bug of pg-2

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
+function smoothScroll() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+// smoothScroll();
+
 function timeLineOne() {
   var tl = gsap.timeline({
     scrollTrigger: {
       trigger: "#home",
       start: "top top",
-      // markers: true,
+      markers: true,
       scrub: 1,
       pin: true,
       end: "bottom -150%",
@@ -274,8 +316,8 @@ timeLineOne();
 function timeLineTwo() {
   const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: "#page-2",
-      start: "top top",
+      trigger: "#main-2",
+      start: "bottom 150%",
       markers: true,
       scrub: 1,
       pin: true,
@@ -343,7 +385,6 @@ function timeLineTwo() {
       "#text h4",
       {
         color: "#000",
-        duration: 1,
         ease: "Power1.easeInOut",
       },
       "hi"
@@ -352,7 +393,6 @@ function timeLineTwo() {
       "#left-bottom-nav h5, #icon",
       {
         color: "#000",
-        duration: 1,
         ease: "Power1.easeInOut",
       },
       "hi"
@@ -361,7 +401,6 @@ function timeLineTwo() {
       "#left-bottom-nav #iso",
       {
         backgroundColor: "#000000cd",
-        duration: 1,
         ease: "Power1.easeInOut",
       },
       "hi"
@@ -370,17 +409,16 @@ function timeLineTwo() {
       "#left-bottom-nav #iso h4",
       {
         color: "#fff",
-        duration: 1,
         ease: "Power1.easeInOut",
       },
       "hi"
-    );
+    )
 
-  // .to(".circle-1", {
-  //   top: "50%",
-  //   duration: 1,
-  //   ease: "Power1",
-  // });
+    .to("#page-2 .circle-1", {
+      top: "50%",
+      duration: 1,
+      ease: "Power1",
+    });
 }
 
 timeLineTwo();
